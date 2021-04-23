6ceb013d334888bd1f5124c2259a9a845cfb284d
Michael Provencher <jangopro@users.noreply.github.com>
2021-04-23 11:40:47 -0400

    config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import "./index.css";
 
+import Login from "./Login";
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-    <React.StrictMode>{/* <App /> */}</React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(<React.StrictMode>{<Login />}</React.StrictMode>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
