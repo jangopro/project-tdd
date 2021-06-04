dcea7199906ffe2346331feea2dc77e32edcbc3c
Jean-Philippe Sirois <jsirois@myca.com>
2021-06-04 10:56:10 -0400

    Reset TDD

--- src/Login.test.jsx
+++ src/Login.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
 import Login from "./Login";
-import userEvent from "@testing-library/user-event";
 
 function renderLogin() {
     render(<Login />);
@@ -31,45 +30,12 @@ describe("display UI and check default state", () => {
     test("should render a submit button", () => {
         expect(getSubmitButton()).toBeInTheDocument();
     });
-
-    test("submit button should be disabled by default", () => {
-        expect(getSubmitButton()).toBeDisabled();
-    });
 });
 
-describe("error state", () => {
-    describe.skip("password ", () => {
-        const testingValues = [
-            [0, true],
-            [1, true],
-            [20, true],
-            [21, false],
-        ];
-        test.each(testingValues)("given %p when %p", (firstArg, booleanValue) => {
-            expect(getPasswordInput().value.length === firstArg).toBe(booleanValue);
-        });
-    });
-});
+describe("error state", () => {});
 
 describe("display error message form state", () => {});
-describe("enable/ disable submit button", () => {
-    test("submit button should be disabled when the password field is empty", () => {
-        userEvent.type(getUsernameInput(), "1234");
-        expect(getSubmitButton()).toBeDisabled();
-    });
 
-    test("submit button should be disabled when the username field is empty", () => {
-        userEvent.type(getPasswordInput(), "1234");
-        expect(getSubmitButton()).toBeDisabled();
-    });
-
-    test("submit button is enabled when both fields have value", () => {
-        userEvent.type(getUsernameInput(), "charles");
-        userEvent.type(getPasswordInput(), "pwd");
-
-        expect(getSubmitButton()).toBeEnabled();
-    });
-});
 describe("actions", () => {});
 
 function getPasswordInput() {
